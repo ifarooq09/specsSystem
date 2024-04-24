2a123e153314003c37969acfe61cc3d6c2ab0419
Ibrahim Farooq <ifarooq9@gmail.com>
2024-04-24 11:45:44 +0430

    debugging

--- backEnd/index.js
+++ backEnd/index.js
@@ -12,11 +12,11 @@ const port = 3000
 
 app.use(express.json());
 
-app.use(cors({
+app.use('*', cors({
     origin: 'http://localhost:5173',
     credentials: true // Allow cookies to be sent with the request
   }));
-  
+
 app.use(cookieParser());
 
 app.use(userRouter);
--- backEnd/middleware/authenticate.js
+++ backEnd/middleware/authenticate.js
@@ -5,23 +5,37 @@ import { key } from "../key.js";
 const authenticate = async (req, res, next) => {
     try {
         const token = req.headers.authorization;
-        
-        const verifyToken = jwt.verify(token, key);
-        
-        const rootUser = await userModel.findOne({_id: verifyToken._id});
-        
-        if(!rootUser) {
-            throw new Error("user not found")
+
+        if (!token) {
+            throw new Error("No token provided");
         }
 
-        req.token = token
-        req.rootUser = rootUser
-        req.userId = rootUser._id
+        const decodedToken = jwt.verify(token, key);
+        
+        const rootUser = await userModel.findOne({ _id: decodedToken._id });
+
+        if (!rootUser) {
+            throw new Error("User not found");
+        }
+
+        // Check token expiration
+        if (Date.now() >= decodedToken.exp * 1000) {
+            throw new Error("Token expired");
+        }
+
+        req.token = token;
+        req.rootUser = rootUser;
+        req.userId = rootUser._id;
 
         next();
     } catch (error) {
-        res.status(401).json({status: 401, message: "Unauthorized no token provided"})
+        if (error.name === 'TokenExpiredError') {
+            res.status(401).json({ status: 401, message: "Token expired" });
+        } else {
+            res.status(401).json({ status: 401, message: error.message || "Unauthorized" });
+        }
     }
 }
+
 
 export default authenticate
